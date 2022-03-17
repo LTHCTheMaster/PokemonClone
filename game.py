e98f92b34bbdd23d543929cd929456cf641fac28
LTHCTheMaster <mistermega52@gmail.com.com>
2022-03-17 17:43:42 +0100

    Add colision with other, and a better camera management

--- game.py
+++ game.py
@@ -1,57 +1,48 @@
 import pygame
+from camera import Camera
+from map import Map
 from player import Player
 from gamestate import GameState
 import configuration
-
-map_tile_image = {
-    "G": pygame.transform.scale(pygame.image.load('assets/imgs/grass0.png').convert_alpha(), (configuration.SCALE, configuration.SCALE)),
-    "H": pygame.transform.scale(pygame.image.load('assets/imgs/grass1.png').convert_alpha(), (configuration.SCALE, configuration.SCALE)),
-    "W": pygame.transform.scale(pygame.image.load('assets/imgs/water.png').convert_alpha(), (configuration.SCALE, configuration.SCALE))
-}
 
 class Game:
     def __init__(self, screen: pygame.Surface):
         self.screen: pygame.Surface = screen
         self.objects: list = []
         self.gamestate = GameState.NONE
-        self.map = []
+        self.map: Map = None
+        self.camera = Camera(0,0)
         self.set_up()
     
     def set_up(self):
+        map_loaded = Map("01")
+        self.map = map_loaded
+
         player = Player(1, 1)
         self.player = player
 
-        self.objects.append(self.player)
-
         self.gamestate = GameState.RUNNING
-        
-        self.load_map("01")
-    
-    def load_map(self, map: str):
-        with open('assets/maps/' + map + '.txt', 'r') as map_file:
-            for line in map_file.readlines():
-                temp = []
-                for i in range(0, len(line) - 1):
-                    temp.append(line[i])
-                self.map.append(temp.copy())
 
     def update(self):
         self.screen.fill(configuration.BLACK)
         self.handle_events()
-        self.render_map()
+        self.camera.determine_camera(self.map, self.player)
+        cam_pos = self.camera.get_pos()
+        self.map.render(self.screen, cam_pos[0], cam_pos[1])
+        self.player.render(self.screen, cam_pos[0], cam_pos[1])
         for object in self.objects:
             object.render(self.screen)
+    
+    def move_object(self, object: Player, pos_moves: list[int]):
+        new_pos = [object.positions[0] + pos_moves[0], object.positions[1] + pos_moves[1]]
 
-    def render_map(self):
-        y_pos = 0
-        for line in self.map:
-            x_pos = 0
-            for tile in line:
-                image = map_tile_image[tile]
-                rect = pygame.Rect(x_pos*configuration.SCALE,y_pos*configuration.SCALE,configuration.SCALE,configuration.SCALE)
-                self.screen.blit(image, rect)
-                x_pos += 1
-            y_pos += 1
+        map_size = self.map.get_size()
+        if new_pos[0] < 0 or new_pos[0] > map_size[0] or new_pos[1] < 0 or new_pos[1] > map_size[1]:
+            return
+        if self.map.get_at(new_pos) == "W":
+            return
+        
+        object.update_position(pos_moves)
 
     def handle_events(self):
         for event in pygame.event.get():
@@ -60,11 +51,11 @@ class Game:
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.gamestate = GameState.ENDED
-                elif event.key == pygame.K_z:
-                    self.player.update_position(0, -1)
+                elif event.key in (pygame.K_z, pygame.K_w):
+                    self.move_object(self.player, [0, -1])
                 elif event.key == pygame.K_s:
-                    self.player.update_position(0, 1)
-                elif event.key == pygame.K_q:
-                    self.player.update_position(-1, 0)
+                    self.move_object(self.player, [0, 1])
+                elif event.key in (pygame.K_q, pygame.K_a):
+                    self.move_object(self.player, [-1, 0])
                 elif event.key == pygame.K_d:
-                    self.player.update_position(1, 0)
+                    self.move_object(self.player, [1, 0])
--- player.py
+++ player.py
@@ -11,9 +11,9 @@ class Player:
     def update(self):
         print("player updated")
     
-    def render(self, screen: pygame.Surface):
+    def render(self, screen: pygame.Surface, x_axis: int, y_axis: int):
+        self.rect = pygame.Rect(self.positions[0]*configuration.SCALE - (x_axis * configuration.SCALE),self.positions[1]*configuration.SCALE - (y_axis * configuration.SCALE),configuration.SCALE,configuration.SCALE)
         screen.blit(self.image, self.rect)
     
-    def update_position(self, pos_x: int, pos_y: int):
-        self.positions = [self.positions[0] + pos_x, self.positions[1] + pos_y]
-        self.rect = pygame.Rect(self.positions[0]*configuration.SCALE,self.positions[1]*configuration.SCALE,configuration.SCALE,configuration.SCALE)
+    def update_position(self, pos_moves: list[int]):
+        self.positions = [self.positions[0] + pos_moves[0], self.positions[1] + pos_moves[1]]
